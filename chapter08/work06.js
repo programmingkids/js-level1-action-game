0aee2ecafda499aceb68b820ef9a9f61ae6bdbfd
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-02-10 06:42:58 +0000

    added chapter08

---
+++ chapter08/work06.js
@@ -0,0 +1,4 @@
+var number1 = 12;
+var number2 = 5;
+
+console.log( number1 % number2 );
---
+++ chapter08/work10.js
@@ -0,0 +1,5 @@
+for( var i = 1; i <= 40; i++ ) {
+    if( i % 5 == 0 ) {
+        console.log( i );
+    }
+}
